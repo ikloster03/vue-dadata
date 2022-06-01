5265fefd58cb3295c828ebca9be99e49b5b473b2
Ivan Monastyrev <ikloster@yandex.ru>
2022-06-01 17:23:56 +0300

    config: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,9 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
+    "plugin:vue/recommended",
+    'airbnb-base',
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
     ecmaVersion: 2020
