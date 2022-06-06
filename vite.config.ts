a71faab5441d8d6b176c12b9ca4e42a85139b41d
Ivan Monastyrev <ikloster@yandex.ru>
2022-06-06 10:09:27 +0300

    add configs

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default () => defineConfig({
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vue-dadata',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
