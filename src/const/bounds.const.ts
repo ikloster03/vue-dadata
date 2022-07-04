a297a3a363c95ba12ef9a49ae7f19c79afb58878
Ivan Monastyrev <ikloster@yandex.ru>
2022-07-04 18:20:45 +0300

    feat(const): add bounds

---
+++ src/const/bounds.const.ts
@@ -0,0 +1,10 @@
+export const BOUNDS = {
+  COUNTRY: 'country' as const,
+  REGION: 'region' as const,
+  AREA: 'area' as const,
+  CITY: 'city' as const,
+  SETTLEMENT: 'settlement' as const,
+  STREET: 'street' as const,
+  HOUSE: 'house' as const,
+  FLAT: 'flat' as const,
+};
