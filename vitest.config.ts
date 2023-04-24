57bea6ab64a0f826f4fe80f41b33ebaddf889c53
Ivan Monastyrev <ikloster@yandex.ru>
2023-04-24 22:28:42 +0300

    feat: remove jest + add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import * as path from 'path';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    coverage: {
+      // provider: 'istanbul',
+      provider: 'c8',
+      reporter: ['text', 'json', 'html'],
+    },
+    include: ['./src/**/*.test.ts']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    vue(),
+  ],
+});
