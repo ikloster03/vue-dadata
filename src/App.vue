0bb9987b9115380727b04945d69b2ef29ea53551
Ivan Monastyrev <ikloster@yandex.ru>
2023-04-24 22:29:15 +0300

    feat: add demo page

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="vue-truncate-html-example">
+    <vue-dadata
+      v-model="query"
+      v-model:suggestion="suggestion"
+      :token="token"
+      :autocomplete="true" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import VueDadata from './VueDadata.vue';
+import './index.css';
+
+export default defineComponent({
+  name: 'VueDadataExample',
+  components: {
+    VueDadata,
+  },
+  setup() {
+    const query = ref('');
+    const suggestion = ref(undefined);
+
+    return {
+      token: import.meta.env.VITE_APP_DADATA_API_KEY as string,
+      query,
+      suggestion,
+    };
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
