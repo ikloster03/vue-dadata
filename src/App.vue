2d57596e04905bf860affbac63cfda9ae5e27b05
Ivan Monastyrev <ikloster@yandex.ru>
2023-04-27 23:40:48 +0300

    feat(app): add highlightOptions example

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <vue-dadata
       v-model="query"
       v-model:suggestion="suggestion"
+      :highlight-options="highlightOptions"
       :token="token"
       :autocomplete="true" />
   </div>
@@ -12,6 +13,7 @@
 import { defineComponent, ref } from 'vue';
 import VueDadata from './VueDadata.vue';
 import './index.css';
+import { HighlightOptions } from './types';
 
 export default defineComponent({
   name: 'VueDadataExample',
@@ -21,11 +23,16 @@ export default defineComponent({
   setup() {
     const query = ref('');
     const suggestion = ref(undefined);
+    const highlightOptions: HighlightOptions = {
+      highlightClass: 'test',
+      highlightTag: 'span',
+    };
 
     return {
       token: import.meta.env.VITE_APP_DADATA_API_KEY as string,
       query,
       suggestion,
+      highlightOptions,
     };
   },
 });
