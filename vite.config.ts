03e91dd1e4129e0170a7abba573f78e8f8585d90
Ivan Monastyrev <ikloster@yandex.ru>
2023-05-19 00:36:17 +0300

    fix: conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import { terser } from 'rollup-plugin-terser';
+import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import pkg from './package.json';
+
+const banner = `/**
+  * ${pkg.name} v${pkg.version}
+  * (c) ${new Date().getFullYear()} ${pkg.author}
+  * @license MIT
+  */\n`
 
 export default () => defineConfig({
   plugins: [vue(), dts()],
@@ -14,13 +22,49 @@ export default () => defineConfig({
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
+      output: [
+        {
+          format: 'es',
+          dir: 'dist',
+          entryFileNames: 'vue-dadata.[format].js',
+          chunkFileNames: '[name].js',
+          banner,
+          plugins: [terser()], // Minify.
         },
-      },
+
+        // Works with dynamic import().
+        {
+          format: 'cjs',
+          dir: 'dist',
+          entryFileNames: 'vue-dadata.[format].js',
+          chunkFileNames: '[name].js',
+          banner,
+        },
+
+        // Works with dynamic import().
+        {
+          format: 'amd',
+          name: 'vuedadata',
+          dir: 'dist',
+          entryFileNames: 'vue-dadata.[format].js',
+          chunkFileNames: '[name].js',
+          // Provide global variables to use in the UMD build for externalized deps.
+          globals: { vue: 'Vue' },
+          banner,
+        },
+
+        // IIFE & UMD don't work with dynamic import().
+        {
+          format: 'iife',
+          name: 'vuedadata',
+          inlineDynamicImports: true, // Everything contained in a single file.
+          dir: 'dist',
+          entryFileNames: 'vue-dadata.[format].js',
+          chunkFileNames: '[name].js',
+          // Provide global variables to use in the UMD build for externalized deps.
+          globals: { vue: 'Vue' }
+        }
+      ]
     },
   },
 });
