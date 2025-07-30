7fe05a909a74a45ca069ce1800357ca4ecc5fc2b
Ivan Monastyrev <ikloster@yandex.ru>
2025-07-30 22:34:01 +0300

    chore: add ESLint and Prettier configuration files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,57 +1,49 @@
 module.exports = {
   root: true,
-  ignorePatterns: [
-    'node_modules',
-    'dist',
-    'dist-ssr',
-    'package-lock.json',
-    'yarn.lock.json',
-    '*.local',
-  ],
+  ignorePatterns: ['node_modules', 'dist', 'dist-ssr', 'package-lock.json', 'yarn.lock.json', '*.local'],
   env: {
     browser: true,
     es2021: true,
+    'vue/setup-compiler-macros': true,
+    jest: true,
+    node: true,
   },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'airbnb-base',
-    '@vue/typescript/recommended',
-
-  ],
+  extends: ['plugin:vue/vue3-recommended', 'airbnb-base', '@vue/typescript/recommended'],
   parser: 'vue-eslint-parser',
   parserOptions: {
-    project: './tsconfig.json',
+    ecmaFeatures: { impliedStrict: true },
+    ecmaVersion: 2021,
+    parser: '@typescript-eslint/parser',
     sourceType: 'module',
-    parser: {
-      ts: '@typescript-eslint/parser',
-    },
-    ecmaVersion: 2018,
-    ecmaFeatures: {
-      globalReturn: false,
-      impliedStrict: false,
-      jsx: false,
+    vueFeatures: {
+      filter: false,
+      interpolationAsNonHTML: true,
     },
   },
+  plugins: ['vue'],
   rules: {
     'no-param-reassign': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-unused-vars': 'warn',
     'consistent-return': 'off',
     'no-underscore-dangle': 'off',
-    'vue/define-emits-declaration': ['error', 'type-based'],
-    'vue/html-closing-bracket-newline': ['error', {
-      singleline: 'never',
-      multiline: 'never',
-    }],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'never',
+      },
+    ],
     'import/prefer-default-export': 'off',
     'import/no-extraneous-dependencies': 'off',
     'padding-line-between-statements': [
       'error',
       { blankLine: 'always', prev: '*', next: 'return' },
       { blankLine: 'always', prev: ['const', 'var', 'let'], next: '*' },
-      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
     ],
     'max-len': ['warn', { code: 150 }],
     'import/extensions': [
@@ -65,8 +57,6 @@ module.exports = {
         '': 'never',
       },
     ],
-    'no-useless-constructor': 'off',
-    'class-methods-use-this': 'off',
   },
   settings: {
     'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
@@ -74,11 +64,9 @@ module.exports = {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
     'import/resolver': {
-      typescript: true,
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
-
 };
---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  vueIndentScriptAndStyle: false,
+};
